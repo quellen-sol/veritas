f9ec2409b1bcd01601af0206d388d021e14a4d71
quellen-sol <quellen@step.finance>
2025-03-03 14:46:06 -0700

    USDT oracle

--- crates/sdk/src/constants.rs
+++ crates/sdk/src/constants.rs
@@ -5,8 +5,10 @@ pub const STEP_MINT: &str = "StepAscQoEioFxxWGnh2sLBDFp9d8rvKz2Yp39iDpyT";
 
 pub const SOL_FEED_ACCOUNT_ID: &str = "7UVimffxr9ow1uXYxsr4LHAcV58mLzhmwaeKvJ1pjLiE";
 pub const USDC_FEED_ACCOUNT_ID: &str = "Dpw1EAVrSB1ibxiDQyTAW6Zip3J4Btk2x4SgApQCeFbX";
+pub const USDT_FEED_ACCOUNT_ID: &str = "HT2PLQBcG5EiCcNSaMHAjSgd9F98ecpATbk4Sk5oYuM";
 
-pub const ORACLE_FEED_MAP_PAIRS: [(&str, &str); 2] = [
+pub const ORACLE_FEED_MAP_PAIRS: [(&str, &str); 3] = [
     (USDC_FEED_ACCOUNT_ID, USDC_MINT),
     (SOL_FEED_ACCOUNT_ID, WSOL_MINT),
+    (USDT_FEED_ACCOUNT_ID, USDT_MINT),
 ];
