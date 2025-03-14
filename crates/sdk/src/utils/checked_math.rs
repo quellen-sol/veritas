ab10ac4f95d1406a3aeb8869bd49e0a7abaf69cf
quellen-sol <quellen@step.finance>
2025-03-14 13:18:11 -0600

    clamp Decimal price values to CH scale

--- crates/sdk/src/utils/checked_math.rs
+++ crates/sdk/src/utils/checked_math.rs
@@ -10,3 +10,25 @@ pub fn checked_pct_diff(old: &Decimal, new: &Decimal) -> Option<Decimal> {
 pub fn is_significant_change(old: &Decimal, new: &Decimal) -> bool {
     checked_pct_diff(old, new).is_some_and(|d| d > Decimal::from_f64(PCT_DIFF_TOLERANCE).unwrap())
 }
+
+/// Try to make this Decimal fit our CH table spec
+pub fn clamp_to_scale(value: &Decimal) -> Decimal {
+    value.normalize().trunc_with_scale(6)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use rust_decimal::Decimal;
+
+    use super::clamp_to_scale;
+
+    #[test]
+    fn clamping() {
+        let value = Decimal::from_str("302.92938638770920892670435169").unwrap();
+        let clamped = clamp_to_scale(&value);
+
+        assert_eq!(clamped, Decimal::from_str("302.929386").unwrap())
+    }
+}
