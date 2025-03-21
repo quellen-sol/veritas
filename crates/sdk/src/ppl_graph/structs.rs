0f1d9c842868f42b71d25b34d13b4a667faed6af
quellen-sol <quellen@step.finance>
2025-03-21 10:25:10 -0600

    liq levels impl for CpLp

--- crates/sdk/src/ppl_graph/structs.rs
+++ crates/sdk/src/ppl_graph/structs.rs
@@ -1,17 +1,22 @@
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
 
-/// When calling `get_price`, this enum describes which token's price to return
-pub enum TokenTarget {
-    Origin,
-    Destination,
+#[derive(Debug)]
+pub struct LiqLevels {
+    /// pct change
+    pub one_sol_depth: Decimal,
+    /// pct change
+    pub ten_sol_depth: Decimal,
+    /// pct change
+    pub thousand_sol_depth: Decimal,
 }
 
-#[derive(Debug, Default)]
-pub struct LiqLevels {
-    pub one_sol_depth: Decimal,
-    pub ten_sol_depth: Decimal,
-    pub thousand_sol_depth: Decimal,
+impl LiqLevels {
+    pub const ZERO: Self = Self {
+        one_sol_depth: Decimal::ZERO,
+        ten_sol_depth: Decimal::ZERO,
+        thousand_sol_depth: Decimal::ZERO,
+    };
 }
 
 #[derive(Serialize, Deserialize)]
@@ -58,18 +63,52 @@ impl LiqRelation {
         }
     }
 
-    // #[inline]
-    // pub fn get_liq_levels(&self) -> LiqLevels {
-    //     match self {
-    //         LiqRelationEnum::CpLp {
-    //             amt_origin: amt_a,
-    //             amt_dest: amt_b,
-    //             ..
-    //         } => {}
-    //     }
-    //
-    //     LiqLevels::default()
-    // }
+    #[inline]
+    pub fn get_liq_levels(&self, tokens_per_sol: Decimal) -> Option<LiqLevels> {
+        match self {
+            LiqRelation::CpLp {
+                amt_origin: amt_a,
+                amt_dest: amt_b,
+            } => {
+                let current_price_a = amt_a.checked_div(*amt_b)?;
+                let product = amt_a.checked_mul(*amt_b)?;
+
+                let tokens_amt_one_sol = tokens_per_sol;
+                let tokens_amt_ten_sol = tokens_amt_one_sol.checked_mul(Decimal::from(10))?;
+                let tokens_amt_thousand_sol =
+                    tokens_amt_one_sol.checked_mul(Decimal::from(1000))?;
+
+                let post_a_one_sol = amt_a.checked_add(tokens_amt_one_sol)?;
+                let post_a_ten_sol = amt_a.checked_add(tokens_amt_ten_sol)?;
+                let post_a_thousand_sol = amt_a.checked_add(tokens_amt_thousand_sol)?;
+
+                let post_b_one_sol = product.checked_div(post_a_one_sol)?;
+                let post_b_ten_sol = product.checked_div(post_a_ten_sol)?;
+                let post_b_thousand_sol = product.checked_div(post_a_thousand_sol)?;
+
+                let price_one_sol = post_a_one_sol.checked_div(post_b_one_sol)?;
+                let price_ten_sol = post_a_ten_sol.checked_div(post_b_ten_sol)?;
+                let price_thousand_sol = post_a_thousand_sol.checked_div(post_b_thousand_sol)?;
+
+                let one_sol_pct_change = price_one_sol
+                    .checked_div(current_price_a)?
+                    .checked_sub(Decimal::ONE)?;
+                let ten_sol_pct_change = price_ten_sol
+                    .checked_div(current_price_a)?
+                    .checked_sub(Decimal::ONE)?;
+                let thousand_sol_pct_change = price_thousand_sol
+                    .checked_div(current_price_a)?
+                    .checked_sub(Decimal::ONE)?;
+
+                Some(LiqLevels {
+                    one_sol_depth: one_sol_pct_change,
+                    ten_sol_depth: ten_sol_pct_change,
+                    thousand_sol_depth: thousand_sol_pct_change,
+                })
+            }
+            LiqRelation::Fixed { .. } => Some(LiqLevels::ZERO),
+        }
+    }
 
     /// Returns `None` if unable to calculate liquidity of this relation (through overflows, divs by 0, etc)
     #[inline]
