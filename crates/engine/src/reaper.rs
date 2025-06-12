76959488c6c2447bfd8a9cdd1bb6275c910981c5
quellen-sol <52678959+quellen-sol@users.noreply.github.com>
2025-06-12 16:08:34 -0600

    exclusive write lock

--- crates/engine/src/reaper.rs
+++ crates/engine/src/reaper.rs
@@ -5,7 +5,7 @@ use veritas_sdk::ppl_graph::graph::WrappedMintPricingGraph;
 
 pub async fn reaper_task(graph: WrappedMintPricingGraph, clickhouse_client: Client) {
     loop {
-        let g_read = graph.read().await;
+        let g_read = graph.write().await; // Write lock for exclusive access to the graph
         for node in g_read.node_weights() {
             let mut p_write = node.usd_price.write().await;
             p_write.take();
