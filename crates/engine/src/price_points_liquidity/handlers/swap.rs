5f2e301e99e293ce3aee54920b5a5dd1aa4e1a61
quellen-sol <52678959+quellen-sol@users.noreply.github.com>
2025-09-24 14:09:22 -0600

    fix logic

--- crates/engine/src/price_points_liquidity/handlers/swap.rs
+++ crates/engine/src/price_points_liquidity/handlers/swap.rs
@@ -136,7 +136,7 @@ pub fn handle_swap_event(
 
     let is_exempt = swap_limit_exempt_set.contains(mint_to_set_price);
     if (is_exempt && oracle_amount_usd < MIN_EXEMPT_SWAP_VOLUME_USD)
-        || oracle_amount_usd < MIN_SWAP_VOLUME_USD
+        || (!is_exempt && oracle_amount_usd < MIN_SWAP_VOLUME_USD)
     {
         return;
     }
